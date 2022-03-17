0268cce7876cf63b962c1c5065bd183fe48921ff
TheShonuff <jirvine124@gmail.com>
2022-03-17 01:18:42 -0700

    layout and code complete. begin bug hunt

--- somejava.js
+++ somejava.js
@@ -1,8 +1,3 @@
-// const data = fetch("./data.json")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error));
-
 const data = [
   {
     title: "Work",
@@ -110,11 +105,20 @@ const data = [
 
 const WorkCurrent = document.querySelector(".workContent .cardInfo");
 const WorkPrevious = document.querySelector(".workContent .previousWeek");
-const Play = document.querySelector(".playContent");
-const Study = document.querySelector(".studyContent");
-const Exercise = document.querySelector(".exerciseContent");
-const Social = document.querySelector(".socialContent");
-const SelfCare = document.querySelector(".selfcareContent");
+const PlayCurrent = document.querySelector(".playContent .cardInfo");
+const PlayPrevious = document.querySelector(".playContent .previousWeek");
+const StudyCurrent = document.querySelector(".studyContent .cardInfo");
+const StudyPrevious = document.querySelector(".studyContent .previousWeek");
+const ExerciseCurrent = document.querySelector(".exerciseContent .cardInfo");
+const ExercisePrevious = document.querySelector(
+  ".exerciseContent .previousWeek"
+);
+const SocialCurrent = document.querySelector(".socialContent .cardInfo");
+const SocialPrevious = document.querySelector(".socialContent .previousWeek");
+const SelfCareCurrent = document.querySelector(".selfcareContent .cardInfo");
+const SelfCarePrevious = document.querySelector(
+  ".selfcareContent .previousWeek"
+);
 
 async function loadJson() {
   const data = await fetch("./data.json")
@@ -125,25 +129,101 @@ async function loadJson() {
 }
 
 function loadDaily() {
-  //   const data = loadJson();
   for (let x = 0; x < data.length; x++) {
     let objTitle = data[x].title;
-    console.log(objTitle);
     let objTimeFrames = data[x].timeframes;
-    console.log(objTimeFrames.daily.current);
-    console.log(objTimeFrames.daily.previous);
 
     if (objTitle === "Work") {
       WorkCurrent.innerHTML = `<h3>${objTimeFrames.daily.current}hrs</h3>`;
       WorkPrevious.innerHTML = `<h4>Yesterday - ${objTimeFrames.daily.previous}hrs</h4>`;
+    }
+    if (objTitle === "Study") {
+      StudyCurrent.innerHTML = `<h3>${objTimeFrames.daily.current}hrs</h3>`;
+      StudyPrevious.innerHTML = `<h4>Yesterday - ${objTimeFrames.daily.previous}hrs</h4>`;
+    }
+    if (objTitle === "Play") {
+      PlayCurrent.innerHTML = `<h3>${objTimeFrames.daily.current}hrs</h3>`;
+      PlayPrevious.innerHTML = `<h4>Yesterday - ${objTimeFrames.daily.previous}hrs</h4>`;
+    }
+    if (objTitle === "Exercise") {
+      ExerciseCurrent.innerHTML = `<h3>${objTimeFrames.daily.current}hrs</h3>`;
+      ExercisePrevious.innerHTML = `<h4>Yesterday - ${objTimeFrames.daily.previous}hrs</h4>`;
+    }
+    if (objTitle === "Social") {
+      SocialCurrent.innerHTML = `<h3>${objTimeFrames.daily.current}hrs</h3>`;
+      SocialPrevious.innerHTML = `<h4>Yesterday - ${objTimeFrames.daily.previous}hrs</h4>`;
+    }
+    if (objTitle === "Self Care") {
+      SelfCareCurrent.innerHTML = `<h3>${objTimeFrames.daily.current}hrs</h3>`;
+      SelfCarePrevious.innerHTML = `<h4>Yesterday - ${objTimeFrames.daily.previous}hrs</h4>`;
     }
   }
 }
 
 //   console.log(data[0]);
 
-function loadWeekly() {}
+function loadWeekly() {
+  for (let x = 0; x < data.length; x++) {
+    let objTitle = data[x].title;
+    let objTimeFrames = data[x].timeframes;
 
-function loadMonthly() {}
+    if (objTitle === "Work") {
+      WorkCurrent.innerHTML = `<h3>${objTimeFrames.weekly.current}hrs</h3>`;
+      WorkPrevious.innerHTML = `<h4>Last Week - ${objTimeFrames.weekly.previous}hrs</h4>`;
+    }
+    if (objTitle === "Study") {
+      StudyCurrent.innerHTML = `<h3>${objTimeFrames.weekly.current}hrs</h3>`;
+      StudyPrevious.innerHTML = `<h4>Last Week - ${objTimeFrames.weekly.previous}hrs</h4>`;
+    }
+    if (objTitle === "Play") {
+      PlayCurrent.innerHTML = `<h3>${objTimeFrames.weekly.current}hrs</h3>`;
+      PlayPrevious.innerHTML = `<h4>Last Week - ${objTimeFrames.weekly.previous}hrs</h4>`;
+    }
+    if (objTitle === "Exercise") {
+      ExerciseCurrent.innerHTML = `<h3>${objTimeFrames.weekly.current}hrs</h3>`;
+      ExercisePrevious.innerHTML = `<h4>Last Week - ${objTimeFrames.weekly.previous}hrs</h4>`;
+    }
+    if (objTitle === "Social") {
+      SocialCurrent.innerHTML = `<h3>${objTimeFrames.weekly.current}hrs</h3>`;
+      SocialPrevious.innerHTML = `<h4>Last Week - ${objTimeFrames.weekly.previous}hrs</h4>`;
+    }
+    if (objTitle === "Self Care") {
+      SelfCareCurrent.innerHTML = `<h3>${objTimeFrames.weekly.current}hrs</h3>`;
+      SelfCarePrevious.innerHTML = `<h4>Last Week - ${objTimeFrames.weekly.previous}hrs</h4>`;
+    }
+  }
+}
+
+function loadMonthly() {
+  for (let x = 0; x < data.length; x++) {
+    let objTitle = data[x].title;
+    let objTimeFrames = data[x].timeframes;
+
+    if (objTitle === "Work") {
+      WorkCurrent.innerHTML = `<h3>${objTimeFrames.monthly.current}hrs</h3>`;
+      WorkPrevious.innerHTML = `<h4>Last Month - ${objTimeFrames.monthly.previous}hrs</h4>`;
+    }
+    if (objTitle === "Study") {
+      StudyCurrent.innerHTML = `<h3>${objTimeFrames.monthly.current}hrs</h3>`;
+      StudyPrevious.innerHTML = `<h4>Last Month - ${objTimeFrames.monthly.previous}hrs</h4>`;
+    }
+    if (objTitle === "Play") {
+      PlayCurrent.innerHTML = `<h3>${objTimeFrames.monthly.current}hrs</h3>`;
+      PlayPrevious.innerHTML = `<h4>Last Month - ${objTimeFrames.monthly.previous}hrs</h4>`;
+    }
+    if (objTitle === "Exercise") {
+      ExerciseCurrent.innerHTML = `<h3>${objTimeFrames.monthly.current}hrs</h3>`;
+      ExercisePrevious.innerHTML = `<h4>Last Month - ${objTimeFrames.monthly.previous}hrs</h4>`;
+    }
+    if (objTitle === "Social") {
+      SocialCurrent.innerHTML = `<h3>${objTimeFrames.monthly.current}hrs</h3>`;
+      SocialPrevious.innerHTML = `<h4>Last Month - ${objTimeFrames.monthly.previous}hrs</h4>`;
+    }
+    if (objTitle === "Self Care") {
+      SelfCareCurrent.innerHTML = `<h3>${objTimeFrames.monthly.current}hrs</h3>`;
+      SelfCarePrevious.innerHTML = `<h4>Last Month - ${objTimeFrames.monthly.previous}hrs</h4>`;
+    }
+  }
+}
 
 // if title =
